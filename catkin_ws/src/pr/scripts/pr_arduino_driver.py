a5a52946950b210fe65ac7b63e1c7d958ee7bf02
0x005c <52650857+0x005c@users.noreply.github.com>
2019-12-01 18:58:59 +0900

    remove pr_robocalc.py

--- catkin_ws/src/pr/scripts/pr_arduino_driver.py
+++ catkin_ws/src/pr/scripts/pr_arduino_driver.py
@@ -10,6 +10,12 @@ import numpy as np
 import numpy.linalg as LA
 from pr_robocalc import R
 
+# (x,y,ω)に対する回転行列
+def R(theta):
+    return [[np.cos(theta), -np.sin(theta), 0],
+            [np.sin(theta), np.cos(theta), 0],
+            [0, 0, 1]]
+
 class ArduinoDriver:
     def __init__(self):
         self.center_to_wheel = float(rospy.get_param('/const/spec/center_to_wheel'))
