42a3cb90bc7576488ad7b4d5958fcf2121122135
Banana <tachikoki@gmail.com>
2020-03-17 12:25:34 +0900

    correct launch for param

--- catkin_ws/src/pr_kick/Arduino/arduino/arduino.ino
+++ catkin_ws/src/pr_kick/Arduino/arduino/arduino.ino
@@ -7,29 +7,32 @@
 #include <ros.h>
 #include <std_msgs/Int16MultiArray.h>
 //debug
-#include <std_msgs/Float64.h>
+//#include <std_msgs/Float64.h>
 
 //constant
-const uint8_t ADDR = 0x14;  //address of AVR on ise-motor-driver
+const uint8_t ADDR = 0x12;  //address of AVR on ise-motor-driver
 const int TOUCH_PIN = 6;    //pin
 const int SOLENOID_PIN = 7; //pin
 const int MAIN_DELAY = 10;   //[millisec]
 
 //global variable
-int pw; //power of moter (-100~100)
-int order_wind = 0; //order of wind rope
-int order_kick = 0; //order of kick
+int pw = 0; //power of moter (-255~255)
+int order_wind   = 0; //order of wind rope
+int order_launch = 0; //order of kick
 
 //function prottype
 void callback(const std_msgs::Int16MultiArray& msg);
 void winding();
+void launching();
 
 //init
 ros::NodeHandle nh;
-ros::Subscriber<std_msgs::Int16MultiArray>  sub("kick_tpc",&callback);
+ros::Subscriber<std_msgs::Int16MultiArray>  sub("kick_order",&callback);
+std_msgs::Int16MultiArray data;
+ros::Publisher pub("kick_fin",&data);
 //debug
-std_msgs::Float64 data;
-ros::Publisher pub("debug_tpc",&data);
+//std_msgs::Float64 debug;
+//ros::Publisher pub_d("debug_tpc",&debug);
 
 IseMotorDriver motor(ADDR);
   
@@ -39,21 +42,20 @@ void setup(){
   //init node
   nh.initNode();
   nh.subscribe(sub);
-  //debug
   nh.advertise(pub);
+  //debug
+  //nh.advertise(pub_d);
 
+  data.data_length = 2;
+  data.data = (int16_t*)malloc(sizeof(int16_t)*2);
+  data.data[0] = 0;
+  data.data[1] = 0;
+  
+  
   //init pin
   pinMode(TOUCH_PIN, INPUT_PULLUP);
   pinMode(SOLENOID_PIN, OUTPUT);
   digitalWrite(SOLENOID_PIN, LOW);
-
-  //init power
-  while(pw==0){
-    nh.spinOnce();
-    delay(MAIN_DELAY);
-  }
-  
-
 }
 
 void loop(){
@@ -63,12 +65,13 @@ void loop(){
   if(order_wind==1) winding();
   
   //kick
-  if(order_kick==1){
-    digitalWrite(SOLENOID_PIN, HIGH);
+  if(order_launch==1){
+    launching();
   }else{
-    digitalWrite(SOLENOID_PIN, LOW);
+    digitalWrite(SOLENOID_PIN,LOW);
   }
-  
+  //debug.data = 2;
+  //pub_d.publish(&debug);
   delay(MAIN_DELAY); 
 }
 
@@ -76,34 +79,59 @@ void loop(){
 void winding(){
   //value of touch sensor (OFF:LOW ON:HIGH) 
   long enc = 0; //value of encoder [step];
-    
+
   //normal rotation
   do{
     nh.spinOnce();
+    if(order_wind<0) goto RESET;
+
     motor.setSpeed(pw);
     enc = motor.encorder();
     delay(MAIN_DELAY);
   }while(digitalRead(TOUCH_PIN) == LOW);
-
-  motor.setSpeed(0);
     
+  motor.setSpeed(0);
+
   //reverse rotation
   do{
     nh.spinOnce();
+    if(order_wind<0) goto RESET;
+
     motor.setSpeed(-pw);
     enc = motor.encorder();
     delay(MAIN_DELAY);
-  }while(enc<0);
+  }while(enc>0);
 
   //finish
+  data.data[0] = 0;
+  pub.publish(&data);
+RESET:
   motor.setSpeed(0);
+  
+}
 
+void launching(){
+  nh.spinOnce();
+  if(order_launch<0) goto RESET;
 
+  //debug.data = 0;
+  //pub_d.publish(&debug);
+
+  digitalWrite(SOLENOID_PIN,HIGH);
+
+  if(digitalRead(TOUCH_PIN)==LOW){
+    data.data[1] = 0;
+    pub.publish(&data);
+    //debug.data = 1;
+    //pub_d.publish(&debug);
+  }
+RESET:
+;
 }
 
 //ros callback function
 void callback(const std_msgs::Int16MultiArray& msg){
   order_wind   = msg.data[0];
-  order_kick   = msg.data[1];
+  order_launch = msg.data[1];
   pw           = msg.data[2];
 }
