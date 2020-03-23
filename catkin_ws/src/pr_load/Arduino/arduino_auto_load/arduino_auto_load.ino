f0d3ccca762c047c790880e587c93f9331f72f26
Banana <tachikoki@gmail.com>
2020-03-23 20:25:02 +0900

    update auto_load

--- catkin_ws/src/pr_load/Arduino/arduino_auto_load/arduino_auto_load.ino
+++ catkin_ws/src/pr_load/Arduino/arduino_auto_load/arduino_auto_load.ino
@@ -29,10 +29,12 @@ const uint8_t ADDR_LIFT  = 0x21; //lift move motor address of AVR
 const uint8_t ADDR_GRASP = 0x22; //grasp motor address of AVR
 const uint8_t ADDR_TEE   = 0x23; //tee motor address of AVR
 //pin setting
-const int PIN_SW_RIGHT = 11;
-const int PIN_SW_LEFT  = 8;
-const int PIN_SW_HOLD  = 2;
-const int PIN_SW_OPEN  = 5;
+const int PIN_SW_RIGHT  = 11;
+const int PIN_SW_LEFT   = 8;
+const int PIN_SW_HOLD   = 2;
+const int PIN_SW_OPEN   = 5;
+const int PIN_SW_TOP    = 4;
+const int PIN_SW_BUTTOM = 9;
 //others
 const int MAIN_DELAY = 10; //[milli sec]
 const int TOUCH_OFF  = LOW;
@@ -53,29 +55,28 @@ int ENC_LIFT_BUTTOM;
 int ENC_TEE_HOLD;
 int ENC_TEE_OPEN;
 ///variable
+int pw_slide = 0;
+int pw_lift  = 0;
+int pw_grasp = 0;
+int pw_tee   = 0;
 long enc_lift = 0;
-long enc_tee = 0;
+long enc_tee  = 0;
 
 //function prototype
 void callback(const std_msgs::Int32MultiArray& msg);
 void get_enc(const std_msgs::Int64MultiArray& msg);
-void finish_task();
-void all_stop();
-void rtoo();
-void hold_hold();
-void open_open();
-void go_right();
-void go_left();
-void go_top();
-void go_buttom();
-void go_middle();
+//void get_param(const std_msgs::Int32MultiArray& msg);
 
 //init
 ros::NodeHandle nh;
 std_msgs::Int32 data_fin;
-ros::Publisher pub("losd_fin",&data_fin);
+ros::Publisher pub("load_fin",&data_fin);
 ros::Subscriber<std_msgs::Int32MultiArray> sub("load_order",&callback);
 ros::Subscriber<std_msgs::Int64MultiArray> sub_enc("enc",&get_enc);
+//ros::Subscriber<std_msgs::Int32MultiArray> sub_param("load_param",&get_param);
+//debug
+std_msgs::Int32 debug;
+ros::Publisher pubD("debug_tpc",&debug);
 
 IseMotorDriver mot_slide(ADDR_SLIDE);
 IseMotorDriver mot_lift(ADDR_LIFT);
@@ -90,7 +91,10 @@ void setup(){
   nh.advertise(pub);
   nh.subscribe(sub);
   nh.subscribe(sub_enc);
-  
+  //nh.subscribe(sub_param);
+  //debug
+  nh.advertise(pubD);
+    
   order_task = 0;
   data_fin.data = 0;
 
@@ -98,12 +102,11 @@ void setup(){
   pinMode(PIN_SW_LEFT,INPUT_PULLUP);
   pinMode(PIN_SW_HOLD,INPUT_PULLUP);
   pinMode(PIN_SW_OPEN,INPUT_PULLUP);
+  pinMode(PIN_SW_TOP,INPUT_PULLUP);
+  pinMode(PIN_SW_BUTTOM,INPUT_PULLUP);
 
   //init actuator
-  mot_slide.setSpeed(0);
-  mot_lift.setSpeed(0);
-  mot_grasp.setSpeed(0);
-  mot_tee.setSpeed(0);  
+  all_stop();
 }
 
 void loop(){
@@ -113,182 +116,283 @@ void loop(){
     case TASK_NOTHING:
       all_stop();
       break;
-    
-    case TASK_STANDING_BY:
-      rtoo();
-      delay(2000);
-      hold_hold();
       
-      if(order_task>0) finish_task(); 
-      break;
+    case TASK_STANDING_BY:
+      go_right();
+      go_top();
+      grasp_open();
+      go_buttom();
+      grasp_hold_time(1500);
+      go_top();
+      //tee_hold();
+     
+      finish_task();
+      break;  
 
     case TASK_SETTING_1:
-      go_right();
       go_buttom();
-      open_open();
+      delay(1000);
+      //tee_open();
+      grasp_open();
       go_top();
       go_left();
-      hold_hold();
-      
-      if(order_task>0) finish_task(); 
+      grasp_hold_time(1500);
+    
+      finish_task();
       break;
-      
+
     case TASK_SETTING_2:
+      //tee_hold();
       go_right();
       go_buttom();
-      open_open();
+      delay(1000);
+      //tee_open();
+      grasp_open();
       go_middle();
       go_left();
-      hold_hold();
+      grasp_hold_time(1500);
       
-      if(order_task>0) finish_task(); 
+      finish_task();
       break;
-      
+
     case TASK_SETTING_3:
+      //tee_hold();
       go_right();
       go_buttom();
-      open_open();
-      go_middle();
+      delay(1000);
+      //tee_open();
+      grasp_open();
+      go_top();
       go_left();
       
-      if(order_task>0) finish_task(); 
+      finish_task();
       break;
-      
-    case EMERGENCY_STOP:
+
+    default:
       all_stop();
       break;
-    
+      
   }
   delay(MAIN_DELAY);
-}
-
-void rtoo(){
-  int slide_pw, lift_pw, grasp_pw, tee_pw;
-
-  while(digitalRead(PIN_SW_RIGHT)==TOUCH_OFF || enc_lift<ENC_LIFT_TOP || digitalRead(PIN_SW_OPEN)==TOUCH_OFF || enc_tee<ENC_TEE_OPEN){
-    nh.spinOnce();
-    if(order_task < 0) break;
-    
-    //enc_lift = mot_lift.encorder();
-    //enc_tee  = mot_tee.encorder();
-    
-    digitalRead(PIN_SW_RIGHT)==TOUCH_OFF ? slide_pw=-MOT_SLIDE_PW : slide_pw=0;
-    enc_lift < ENC_LIFT_TOP              ?  lift_pw=MOT_RAISE_PW  :  lift_pw=0;
-    digitalRead(PIN_SW_OPEN)==TOUCH_OFF  ? grasp_pw=-MOT_GRASP_PW : grasp_pw=0;
-    enc_tee  < ENC_TEE_OPEN              ?   tee_pw=MOT_TEE_PW    :   tee_pw=0;   
-    
-    mot_slide.setSpeed(slide_pw);
-    mot_lift.setSpeed(lift_pw);
-    mot_grasp.setSpeed(grasp_pw);
-    mot_tee.setSpeed(tee_pw); 
-
-    delay(MAIN_DELAY);
-  }
-
-  all_stop();
-}
-
-void hold_hold(){
-  int grasp_pw, tee_pw;
-
-  while(digitalRead(PIN_SW_HOLD)==TOUCH_OFF || enc_tee>ENC_TEE_HOLD){
-    nh.spinOnce();
-    if(order_task < 0) break;
-    
-    //enc_tee = mot_tee.encorder();
-    
-    digitalRead(PIN_SW_HOLD)==TOUCH_OFF ? grasp_pw=MOT_GRASP_PW : grasp_pw=0;
-    enc_tee > ENC_TEE_HOLD              ?   tee_pw=-MOT_TEE_PW  :   tee_pw=0;   
-    
-    mot_grasp.setSpeed(grasp_pw);
-    mot_tee.setSpeed(tee_pw); 
-
-    delay(MAIN_DELAY);
-  }  
-  mot_grasp.setSpeed(0);
-  mot_tee.setSpeed(0);
-}
-
-void open_open(){
-  int grasp_pw, tee_pw;
-
-  while(digitalRead(PIN_SW_OPEN)==TOUCH_OFF || enc_tee<ENC_TEE_OPEN){
-    nh.spinOnce();
-    if(order_task < 0) break;
-    
-    //enc_tee  = mot_tee.encorder();
-    
-    digitalRead(PIN_SW_OPEN)==TOUCH_OFF  ? grasp_pw=-MOT_GRASP_PW : grasp_pw=0;
-    enc_tee  < ENC_TEE_OPEN              ?   tee_pw=MOT_TEE_PW    :   tee_pw=0;   
-    
-    mot_grasp.setSpeed(grasp_pw);
-    mot_tee.setSpeed(tee_pw); 
-
-    delay(MAIN_DELAY);
-  }  
-  mot_grasp.setSpeed(0);
-  mot_tee.setSpeed(0);
 }
 
 void go_right(){
   while(digitalRead(PIN_SW_RIGHT)==TOUCH_OFF){
     nh.spinOnce();
     if(order_task < 0) break;
-    mot_slide.setSpeed(-MOT_SLIDE_PW);
+    move_mot_slide(-MOT_SLIDE_PW);
     delay(MAIN_DELAY);
   }
-  mot_slide.setSpeed(0);
+  move_mot_slide(0);
 }
 
 void go_left(){
   while(digitalRead(PIN_SW_LEFT)==TOUCH_OFF){
     nh.spinOnce();
     if(order_task < 0) break;
-    mot_slide.setSpeed(MOT_SLIDE_PW);
+    move_mot_slide(MOT_SLIDE_PW);
     delay(MAIN_DELAY);
   }
-  mot_slide.setSpeed(0);
+  move_mot_slide(0);
 }
 
 void go_top(){
-  while(enc_lift<ENC_LIFT_TOP){
+  while(digitalRead(PIN_SW_TOP)==TOUCH_OFF){
     nh.spinOnce();
     if(order_task < 0) break;
-    mot_lift.setSpeed(MOT_RAISE_PW);
+    move_mot_lift(MOT_RAISE_PW);
     delay(MAIN_DELAY);
   }
-  mot_slide.setSpeed(0);
+  move_mot_lift(0);
 }
 
 void go_buttom(){
-  while(enc_lift>ENC_LIFT_BUTTOM){
+  while(digitalRead(PIN_SW_BUTTOM)==TOUCH_OFF){
     nh.spinOnce();
     if(order_task < 0) break;
-    mot_lift.setSpeed(MOT_LOWER_PW);
+    move_mot_lift(MOT_LOWER_PW);
     delay(MAIN_DELAY);
   }
-  mot_slide.setSpeed(0);
+  move_mot_lift(0);
 }
 
 void go_middle(){
   while(enc_lift<ENC_LIFT_MIDDLE){
     nh.spinOnce();
     if(order_task < 0) break;
-    mot_lift.setSpeed(MOT_RAISE_PW);
+    move_mot_lift(MOT_RAISE_PW);
     delay(MAIN_DELAY);
   }
-  mot_slide.setSpeed(0);
+  move_mot_lift(0);
+}
+
+void grasp_open(){
+  while(digitalRead(PIN_SW_OPEN)==TOUCH_OFF){
+    nh.spinOnce();
+    if(order_task < 0) break;
+    move_mot_grasp(MOT_GRASP_PW);
+    delay(MAIN_DELAY);
+  }
+  move_mot_grasp(0);
+}
+
+void grasp_hold(){
+  while(digitalRead(PIN_SW_HOLD)==TOUCH_OFF){
+    nh.spinOnce();
+    if(order_task < 0) break;
+    move_mot_grasp(-MOT_GRASP_PW);
+    delay(MAIN_DELAY);
+  }
+  move_mot_grasp(0);
+}
+
+void grasp_hold_time(int delay_time){
+  long tm;
+  long beg;
+  tm  = millis();
+  beg = millis();
+  while(tm-beg < delay_time){
+    nh.spinOnce();
+    if(order_task < 0) break;
+    move_mot_grasp(-MOT_GRASP_PW);
+    delay(MAIN_DELAY);
+    tm = millis();
+  }
+  move_mot_grasp(0);
+}
+
+void tee_open(){
+  while(enc_tee < ENC_TEE_OPEN){
+    nh.spinOnce();
+    if(order_task < 0) break;
+    move_mot_tee(MOT_TEE_PW);
+    delay(MAIN_DELAY);
+  }
+  move_mot_tee(0);
+}
+
+void tee_hold(){
+  while(ENC_TEE_HOLD < enc_tee){
+    nh.spinOnce();
+    if(order_task < 0) break;
+    move_mot_tee(-MOT_TEE_PW);
+    delay(MAIN_DELAY);
+  }
+  move_mot_tee(0);
+}
+
+//void rtoo(){
+//  while(digitalRead(PIN_SW_RIGHT)==TOUCH_OFF || enc_lift<ENC_LIFT_TOP || digitalRead(PIN_SW_OPEN)==TOUCH_OFF || enc_tee<ENC_TEE_OPEN){
+//    nh.spinOnce();
+//    if(order_task < 0) break;
+//    
+//    //enc_lift = mot_lift.encorder();
+//    //enc_tee  = mot_tee.encorder();
+//    
+//    digitalRead(PIN_SW_RIGHT)==TOUCH_OFF ? pw_slide=-MOT_SLIDE_PW : pw_slide=0;
+//    enc_lift < ENC_LIFT_TOP              ?  pw_lift=MOT_RAISE_PW  :  pw_lift=0;
+//    digitalRead(PIN_SW_OPEN)==TOUCH_OFF  ? pw_grasp=-MOT_GRASP_PW : pw_grasp=0;
+//    enc_tee  < ENC_TEE_OPEN              ?   pw_tee=MOT_TEE_PW    :   pw_tee=0;   
+//    
+//    mot_slide.setSpeed(pw_slide);
+//    mot_lift.setSpeed(pw_lift);
+//    mot_grasp.setSpeed(pw_grasp);
+//    mot_tee.setSpeed(pw_tee); 
+//    delay(MAIN_DELAY);
+//  }
+//}
+//
+//void hold_hold(){
+//  while(digitalRead(PIN_SW_HOLD)==TOUCH_OFF || enc_tee>ENC_TEE_HOLD){
+//    nh.spinOnce();
+//    if(order_task < 0) break;
+//    
+//    //enc_tee = mot_tee.encorder();
+//    
+//    digitalRead(PIN_SW_HOLD)==TOUCH_OFF ? pw_grasp=MOT_GRASP_PW : pw_grasp=0;
+//    enc_tee > ENC_TEE_HOLD              ?   pw_tee=-MOT_TEE_PW  :   pw_tee=0;   
+//    
+//    mot_grasp.setSpeed(pw_grasp);
+//    nh.spinOnce();
+//    mot_tee.setSpeed(pw_tee); 
+//    nh.spinOnce();
+//
+//    delay(MAIN_DELAY);
+//  }  
+//  mot_grasp.setSpeed(0);
+//  mot_tee.setSpeed(0);
+//}
+//
+//void open_open(){
+//  while(digitalRead(PIN_SW_OPEN)==TOUCH_OFF || enc_tee<ENC_TEE_OPEN){
+//    nh.spinOnce();
+//    if(order_task < 0) break;
+//    
+//    //enc_tee  = mot_tee.encorder();
+//    
+//    digitalRead(PIN_SW_OPEN)==TOUCH_OFF  ? pw_grasp=-MOT_GRASP_PW : pw_grasp=0;
+//    enc_tee  < ENC_TEE_OPEN              ?   pw_tee=MOT_TEE_PW    :   pw_tee=0;   
+//    
+//    mot_grasp.setSpeed(pw_grasp);
+//    mot_tee.setSpeed(pw_tee); 
+//
+//    delay(MAIN_DELAY);
+//  }  
+//  mot_grasp.setSpeed(0);
+//  mot_tee.setSpeed(0);
+//}
+
+void move_mot_slide(int pw){
+  if(pw_slide != pw){
+      pw_slide = pw;
+      mot_slide.setSpeed(pw_slide);
+      nh.spinOnce();
+    }
+}
+
+void move_mot_lift(int pw){
+  if(pw_lift != pw){
+      pw_lift = pw;
+      mot_lift.setSpeed(pw_lift);
+      nh.spinOnce();
+    }
+}
+
+void move_mot_grasp(int pw){
+  if(pw_grasp != pw){
+      pw_grasp = pw;
+      mot_grasp.setSpeed(pw_grasp);
+      nh.spinOnce();
+    }
+}
+
+void move_mot_tee(int pw){
+  if(pw_tee != pw){
+      pw_tee = pw;
+      mot_tee.setSpeed(pw_tee);
+      nh.spinOnce();
+    }
 }
 
 void all_stop(){
-  mot_slide.setSpeed(0);
-  mot_lift.setSpeed(0);
-  mot_grasp.setSpeed(0);
-  mot_tee.setSpeed(0);
+  if(!((pw_slide==0) && (pw_lift==0) && (pw_grasp==0) && (pw_tee==0))){
+    pw_slide = 0;
+    pw_lift  = 0;
+    pw_grasp = 0;
+    pw_tee   = 0;
+    mot_slide.setSpeed(pw_slide); nh.spinOnce();
+    mot_lift.setSpeed(pw_lift);   nh.spinOnce();
+    mot_grasp.setSpeed(pw_grasp); nh.spinOnce();
+    mot_tee.setSpeed(pw_tee);     nh.spinOnce();
+  }
 }
+
 void finish_task(){
-  data_fin.data = 0;
-  pub.publish(&data_fin);
+  if(order_task>0){
+    data_fin.data = 0;
+    pub.publish(&data_fin);
+    order_task = 0; 
+  }
 }
 
 //ros callback function
@@ -310,3 +414,16 @@ void get_enc(const std_msgs::Int64MultiArray& msg){
   enc_lift = msg.data[0];
   enc_tee  = msg.data[1];
 }
+
+//void get_param(const std_msgs::Int32MultiArray& msg){
+//  MOT_SLIDE_PW    = msg.data[0];
+//  MOT_RAISE_PW    = msg.data[1];
+//  MOT_LOWER_PW    = msg.data[2];
+//  MOT_GRASP_PW    = msg.data[3];
+//  MOT_TEE_PW      = msg.data[4];
+//  ENC_LIFT_TOP    = msg.data[5];
+//  ENC_LIFT_MIDDLE = msg.data[6];
+//  ENC_LIFT_BUTTOM = msg.data[7];
+//  ENC_TEE_HOLD    = msg.data[8];
+//  ENC_TEE_OPEN    = msg.data[9];
+//}
--- catkin_ws/src/pr_load/src/load_ball_node.cpp
+++ catkin_ws/src/pr_load/src/load_ball_node.cpp
@@ -8,6 +8,8 @@ ros::Subscriber sub_beg; //subscriber from upper layer (tpc"load_tpc")
 ros::Subscriber sub_fin; //subscriber from arduino (tpc"load_fin")
 ros::Publisher  pub_beg; //publisher to arduino (tpc"load_order")
 ros::Publisher  pub_fin; //publisher to upper layer (tpc"load_tpc")
+//ros::Publisher  pub_param;
+
 int FREQ = 100;  //frequency of main loop [Hz]
 int MOT_SLIDE_PW;
 int MOT_RAISE_PW;
@@ -25,6 +27,8 @@ void cb_begin_task(const std_msgs::Int32& beg_order);
 void cb_finish_task(const std_msgs::Int32& fin_msg);
 
 int main(int argc, char **argv){
+	int count = 0;
+	
 	//node init
 	ros::init(argc,argv,"load_ball_node");
 	ros::NodeHandle nh;
@@ -33,8 +37,10 @@ int main(int argc, char **argv){
 	sub_fin = nh.subscribe("load_fin",10,cb_finish_task); 
 	pub_beg = nh.advertise <std_msgs::Int32MultiArray>("load_order",1);
 	pub_fin = nh.advertise <std_msgs::Int32>("load_tpc",1);
+	//pub_param = nh.advertise <std_msgs::Int32MultiArray>("load_param",1);
 
 	//parameter
+	//std_msgs::Int32MultiArray data_param;
 	nh.getParam("load_ball_node/MOT_SLIDE_PW",MOT_SLIDE_PW);
 	nh.getParam("load_ball_node/MOT_RAISE_PW",MOT_RAISE_PW);
 	nh.getParam("load_ball_node/MOT_LOWER_PW",MOT_LOWER_PW);
@@ -46,11 +52,24 @@ int main(int argc, char **argv){
 	nh.getParam("load_ball_node/ENC_TEE_HOLD",ENC_TEE_HOLD);
 	nh.getParam("load_ball_node/ENC_TEE_OPEN",ENC_TEE_OPEN);
 
+//	data_param.data.resize(10);
+//	data_param.data[0] = MOT_SLIDE_PW;
+//	data_param.data[1] = MOT_RAISE_PW;
+//	data_param.data[2] = MOT_LOWER_PW;
+//	data_param.data[3] = MOT_GRASP_PW;
+//	data_param.data[4] = MOT_TEE_PW;
+//	data_param.data[5] = ENC_LIFT_TOP;
+//	data_param.data[6] = ENC_LIFT_MIDDLE;
+//	data_param.data[7] = ENC_LIFT_BUTTOM;
+//	data_param.data[8] = ENC_TEE_HOLD;
+//	data_param.data[9] = ENC_TEE_OPEN;
+
 	ros::Rate loop_rate(FREQ);
 
 	//node body
 	while(ros::ok()){
 		ros::spinOnce();
+		//pub_param.publish(data_param);
 		loop_rate.sleep();
 	}
 
@@ -83,3 +102,4 @@ void cb_finish_task(const std_msgs::Int32& fin_msg){
 	
 	pub_fin.publish(data);
 }
+
